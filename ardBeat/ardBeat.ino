3571cd783e983613bb0ea98ffc6a209e013045e2
Uthmanhere <uthmanhere@gmail.com>
2017-05-21 13:00:29 +0500

    Create ardBeat.ino

---
+++ ardBeat/ardBeat.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // set the pinOnArtery wire on your wrist
+  // along with either of the available
+  // voltage source.
+  pinMode(pinOnArtery, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // inputs the data to analyaze it latter.
+  Serial.println(analogRead(pinOnArtery));
+  delay(10);
+}
